1a1c966342c56ddb75282a798ceac886e5f65281
Paul Herron <contact@paulherron.net>
2018-06-20 22:35:37 +0100

    Use separate status file for each device

--- server.py
+++ server.py
@@ -7,43 +7,66 @@ import sys
 app = Flask(__name__)
 CORS(app)
 
-status_file_path = os.path.dirname(__file__) + '/status.json'
+status_file_path_template = os.path.dirname(__file__) + '/status_device_%s.json'
 
 @app.route('/')
 def status():
-    return get_status()
+    device = get_device()
+    return get_status(device)
 
 @app.route('/on')
 def on():
-    command = '/home/pi/pihat/pihat --brand=5 --id=' + get_device() + ' --channel=7 --state=1'
+    device = get_device()
+    command = get_command(device, True)
     print command
     os.system(command)
-    update_status('on')
-    return get_status()
+    update_status(device, 'on')
+    return get_status(device)
 
 @app.route('/off')
 def off():
-    command = '/home/pi/pihat/pihat --brand=5 --id=' + get_device() + ' --channel=6 --state=1'
+    device = get_device()
+    command = get_command(device, False)
     print command
     os.system(command)
-    update_status('off')
-    return get_status()
+    update_status(device, 'off')
+    return get_status(device)
 
-def update_status(status):
-    with open(status_file_path, 'w') as status_file:
-        json.dump({'lastCommand': status, 'lastCommandTime': datetime.datetime.now().isoformat()}, status_file)
-
-def get_status():
+def get_status(device):
+    status_file_path = status_file_path_template % (device)
     with open(status_file_path, 'r') as status_file:
         return Response(status_file.read(), mimetype='application/json')
 
+def update_status(device, status):
+    status_file_path = status_file_path_template % (device)
+    with open(status_file_path, 'w') as status_file:
+        json.dump({'lastCommand': status, 'lastCommandTime': datetime.datetime.now().isoformat()}, status_file)
+
 def get_device():
+    """Gets a string to identify the device
+
+    Returns:
+        str: Device string provided in URL, or '0' as a default
+    """
     try:
         device = str(int(request.args.get('device')))
     except (ValueError, TypeError):
         device = '0'
 
     return device
+
+def get_command(device, status):
+    """Gets the pihat command to run
+
+    Args:
+        device (str): device ID, e.g. '0'
+        status (bool): whether to turn on or off
+    Returns:
+        str: Command
+    """
+    channel = '7' if status else '6'
+
+    return '/home/pi/pihat/pihat --brand=5 --id=' + device + ' --channel=' + channel + ' --state=1'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3100)
